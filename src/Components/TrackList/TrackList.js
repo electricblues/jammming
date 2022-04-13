bba85586610505cd51601607b0bc9593085ec11f
electricblues <65368545+electricblues@users.noreply.github.com>
2022-04-13 23:20:49 +0100

    Pass Dwn. Search Rslt. & Render Rslt. Li. (29-35)

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 import "./TrackList.css";
-// import Track from "../Track/";
+
+import Track from "../Track/Track";
 
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* <h1>
-          You will add a map method that renders a set of Track components
-        </h1> */}
+        {this.props.tracks.map((track) => {
+          return <Track track={track} key={track.id} />;
+        })}
       </div>
     );
   }
