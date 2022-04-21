fc42ab09f1c9a5499179d0507cf2654ff88b6532
electricblues <65368545+electricblues@users.noreply.github.com>
2022-04-21 14:25:24 +0100

    Mthd. that Svs. the Plylst to a User's Acc.(62-65)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -55,6 +55,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -76,6 +77,10 @@ class App extends React.Component {
     this.setState({ playlistName: name });
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map((track) => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -94,6 +99,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
